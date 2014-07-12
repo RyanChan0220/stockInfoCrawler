1b2ee8e030e048f939a49cf472e8e44f3457ae74
RyanChan0220 <chenrong0220@gmail.com>
2014-07-12 23:24:35 +0800

    download daily transaction to excel

--- stockInfoCrawler/test.py
+++ stockInfoCrawler/test.py
@@ -14,12 +14,10 @@ def cov_sec2date(sec):
 if __name__ == '__main__':
     xls_url = 'http://market.finance.sina.com.cn/downxls.php?date=2014-01-01&symbol=sz300104'
     dir = '../../stockInfoCrawler//trans/'
-    filename = 'd:/trans/test.xls'
     if os.path.isdir(dir):
         pass
     else:
         os.mkdir(dir)
-    urllib.urlretrieve(xls_url, filename)
     sec = time.time()
     today = date(2014, 7, 12)
     tom = datetime.datetime.strptime("2014-07-12", "%Y-%m-%d")
